09f0c82dc60b60609c09d2ea7f852c75f15fd820
devohda <fffvvv6@gmail.com>
2021-07-11 18:59:09 +0900

    delete if statement

--- apiserver/app.py
+++ apiserver/app.py
@@ -167,6 +167,4 @@ class CommonWords(Resource):
             "tags" : tags
         }
 
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+app.run(debug=True, host='0.0.0.0', port=5000)
